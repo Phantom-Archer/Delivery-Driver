d1359c73c8c3586f60f6d23c866bf0aa6b62be6d
PhantomArcher <tristan.hedges96@yahoo.com>
2022-05-18 18:09:38 -0400

    Added basic collision

--- Assets/Scenes/Car/Driver.cs
+++ Assets/Scenes/Car/Driver.cs
@@ -14,10 +14,10 @@ public class Driver : MonoBehaviour
 
     void Drive()
     {
-        var steerFactor = -steerSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        var steerFactor = steerSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
         var moveFactor = moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
         
         transform.Translate(0f, moveFactor, 0f);
-        transform.Rotate(0f, 0f, steerFactor);
+        transform.Rotate(0f, 0f, -steerFactor);
     }
 }
