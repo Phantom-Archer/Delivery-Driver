cff1598f2bae88fac97d007cc7c8749cff84cb63
PhantomArcher <tristan.hedges96@yahoo.com>
2022-05-18 20:14:51 -0400

    Camera now follows player
    Added more content to the sandbox map
    There's now an invis wall around the map
    More houses, trees, road, and rocks added

---
+++ Assets/Scenes/Camera/FollowCamera.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] Transform followTarget;
+    
+    Vector3 offset;
+    void Start()
+    {
+        offset = new Vector3(0, 0, -10);
+    }
+    void Update()
+    {
+        
+        transform.position = followTarget.transform.position + offset;
+    }
+}
